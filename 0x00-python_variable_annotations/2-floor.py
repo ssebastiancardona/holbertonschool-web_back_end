2eb2a2f6c2ed969e839e47abaf457d3e82d59411
ssebastiancardona <ssebastiancardona@gmail.com>
2022-02-05 21:40:45 -0500

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' 2. floor '''
+
+import math
+
+
+def floor(n: float) -> int:
+    '''toma un float n como argumento
+    y devuelve el piso del flotador'''
+    return math.floor(n)
