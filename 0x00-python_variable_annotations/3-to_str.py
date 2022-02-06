b7dbf54b58975901be3aeaa1d5052d8482b804ac
ssebastiancardona <ssebastiancardona@gmail.com>
2022-02-05 21:43:57 -0500

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''3 str '''
+
+
+def to_str(n: float) -> str:
+    '''toma un float n como argumento y devuelve
+    la representación de cadena del flotador.'''
+    return str(n)
