7f76e44e77b53b02edf61c74704f2ae87c9c03da
ssebastiancardona <ssebastiancardona@gmail.com>
2022-02-05 21:38:05 -0500

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' 1 concat '''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''toma una cadena str1 y una cadena str2 como argumentos
+    y devuelve una cadena concatenada'''
+    return str1 + str2
