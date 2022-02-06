f02ea87be6032eb1b58b010d07a1ae4df1938b03
ssebastiancardona <ssebastiancardona@gmail.com>
2022-02-05 22:26:47 -0500

    0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
-"""modulo de espera
-medio loco"""
+"""0 basic async"""
+
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """espera numero aleatorio
-    Argumentos:
-        max_delay (int, opcional): número máximo. El valor predeterminado es 10.
-    Devoluciones:
-        flotante: número flotante aleatorio
-    """
-    random_delay: float = random.uniform(0, max_delay)
-    await asyncio.sleep(random_delay)
-    return random_delay
+    """funcion
+    random segundos"""
+    segundos = random.uniform(0, max_delay)
+    await asyncio.sleep(segundos)
+    return segundos
