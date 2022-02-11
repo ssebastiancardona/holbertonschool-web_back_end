39eb20d7c51abb42846f9d17c09d3c41031a6df2
ssebastiancardona <ssebastiancardona@gmail.com>
2022-02-10 22:59:50 -0500

    0-basic_cache.py

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-"""modulo de cache basico"""
-from base_caching import BaseCaching
+""" 0. Basic dictionary
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """Clase BasicCache
-    Argumentos:
-        BaseCaching (clase): clase básica para esta clase
+    """ BaseCache defines:
+      - overwrite functions 'put' and 'get'
     """
 
     def put(self, key, item):
-        """poner un nuevo valor en el diccionario cache_data
-        Argumentos:
-            clave ([tipo]): clave del diccionario self.cache_data
-            elemento ([tipo]): valor de la clave
+        """
+        Assign to the dictionary self.cache_data the item
+        value for the key key
         """
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """Obtener el valor del diccionario cache_data
-        Argumentos:
-            clave ([tipo]): clave para buscar en cache_data
         """
-        if not key or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+            Return the value in self.cache_data linked to key
+        """
+        return self.cache_data.get(key)
