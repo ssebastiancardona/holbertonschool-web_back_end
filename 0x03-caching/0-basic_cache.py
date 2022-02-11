6647d795483b32daa0e0425945f3241bb8adb545
ssebastiancardona <ssebastiancardona@gmail.com>
2022-02-10 22:01:23 -0500

    0-basic_cache.py

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""función auxiliar sencilla"""
+"""función auxiliar """
 from typing import Tuple
 
 
-def index_range(pag: int, pag_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """devuelve una tupla de tamaño dos que contiene un índice inicial y un índice final
     Argumentos:
         página (int): número de página
@@ -11,8 +11,8 @@ def index_range(pag: int, pag_size: int) -> Tuple[int, int]:
     Devoluciones:
         Tupla[int, int]: (índice inicial, índice final)
     """
-    end: int = pag * pag_size
-    estart: int = 0
-    for _ in range(pag - 1):
-        estart += pag_size
-    return (estart, end)
+    end: int = page * page_size
+    start: int = 0
+    for _ in range(page - 1):
+        start += page_size
+    return (start, end)
