b279c828c19b5e94fb4975f9a34e4ce40b704607
ssebastiancardona <ssebastiancardona@gmail.com>
2022-03-17 22:41:53 -0500

    101-average_weighted_score.sql

---
+++ 0x0C-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,19 @@
+-- sql languaje holbsc
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers() BEGIN
+delimiter |
+UPDATE
+	users
+SET
+	average_score = (
+		SELECT
+			sum(p.weight * c.score) / sum(p.weight)
+		FROM
+			projects AS p
+			INNER JOIN corrections AS c ON p.id = c.project_id
+		WHERE
+			c.user_id = users.id
+	);
+
+END;
+|
+/ /
