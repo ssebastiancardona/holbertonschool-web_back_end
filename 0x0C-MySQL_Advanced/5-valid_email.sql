0f39f3e046565ef0ba95f3ad1d376a7c178fdcc9
ssebastiancardona <ssebastiancardona@gmail.com>
2022-03-17 22:23:38 -0500

    5-valid_email.sql

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- sql languaje holbsc
+delimiter / / CREATE TRIGGER email_validation BEFORE
+UPDATE
+	ON users FOR EACH ROW BEGIN IF NEW.email <> OLD.email THEN
+SET
+	NEW.valid_email = 0;
+
+
+END IF;
+
+END;
+
+/ /
