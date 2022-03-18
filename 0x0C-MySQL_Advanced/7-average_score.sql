523931c7ca1cb7a9f7769a895eaa0376d6236480
ssebastiancardona <ssebastiancardona@gmail.com>
2022-03-17 22:36:01 -0500

    7-average_score.sql

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -1,19 +1,23 @@
 -- sql languaje holbsc
-delimiter / / CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT) BEGIN
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT) BEGIN DECLARE avg_score FLOAT;
+
+SET
+	avg_score = (
+		SELECT
+			AVG(score)
+		FROM
+			corrections AS C
+		WHERE
+			C.user_id = user_id
+	);
+
 UPDATE
 	users
 SET
-	average_score = (
-		SELECT
-			SUM(score) / COUNT(*)
-		FROM
-			corrections AS c
-		WHERE
-			c.user_id = user_id
-	)
+	average_score = avg_score
 WHERE
 	id = user_id;
 
 END;
-
-/ /
