9f79d76fa17bd2aa0328f5156bd373cf6d7da8ea
ssebastiancardona <ssebastiancardona@gmail.com>
2022-03-24 22:38:26 -0500

    1-block-scoped.js

---
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+export default function taskBlock(trueOrFalse) {
+    const task0 = false;
+    const task1 = true;
+
+    if (trueOrFalse) {
+        // lin
+        const task = true;
+        // ne
+        const task1 = false;
+    }
+
+    return [task0, task1];
+}
