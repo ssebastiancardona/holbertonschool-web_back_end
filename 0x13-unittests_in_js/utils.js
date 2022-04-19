a02f0794c25dd1b27ea08d150ce62cfc8178bbe8
ssebastiancardona <ssebastiancardona@gmail.com>
2022-04-18 23:12:07 -0500

    utils.js

---
+++ 0x13-unittests_in_js/utils.js
@@ -0,0 +1,15 @@
+'use strict';
+const Utils = {
+  calculateNumber(type, a, b) {
+    const operation = {
+      'SUM': (a, b) => Math.round(a) + Math.round(b),
+      'SUBTRACT': (a, b) => Math.round(a) - Math.round(b),
+      'DIVIDE': (a, b) => {
+        if (Math.round(b) === 0) return 'Error';
+        return Math.round(a) / Math.round(b)
+      },
+    }
+    return operation[type](a, b)
+  }
+}
+module.exports = Utils;
